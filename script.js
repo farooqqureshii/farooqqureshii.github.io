ffa7c5b4068fae25c568fc8fd05ec4b2c6266e15
Farooq <156860872+farooqqureshii@users.noreply.github.com>
2024-07-27 23:27:06 -0600

    Update script.js

--- script.js
+++ script.js
@@ -7,8 +7,10 @@ document.addEventListener('DOMContentLoaded', function() {
     const typingTextElement = document.querySelector('.typing-text');
     let textIndex = 0;
     let charIndex = 0;
-    const typingSpeed = 100;
+    const typingSpeed = 65;
+    const deletingSpeed = 70;
     const delayBetweenTexts = 2000;
+    const delayAfterDelete = 1000;
 
     function type() {
         if (charIndex < texts[textIndex].length) {
@@ -16,12 +18,18 @@ document.addEventListener('DOMContentLoaded', function() {
             charIndex++;
             setTimeout(type, typingSpeed);
         } else {
-            setTimeout(() => {
-                typingTextElement.textContent = '';
-                charIndex = 0;
-                textIndex = (textIndex + 1) % texts.length;
-                type();
-            }, delayBetweenTexts);
+            setTimeout(deleteText, delayAfterDelete);
+        }
+    }
+
+    function deleteText() {
+        if (charIndex > 0) {
+            typingTextElement.textContent = texts[textIndex].substring(0, charIndex - 1);
+            charIndex--;
+            setTimeout(deleteText, deletingSpeed);
+        } else {
+            textIndex = (textIndex + 1) % texts.length;
+            setTimeout(type, delayBetweenTexts);
         }
     }
 
@@ -45,16 +53,16 @@ document.addEventListener('DOMContentLoaded', function() {
         darkModeToggle.src = isDarkMode ? 'sun.svg' : 'moon.svg';
         localStorage.setItem('theme', isDarkMode ? 'dark' : 'light');
     });
-});
 
-const footer = document.querySelector('.footer');
+    const footer = document.querySelector('.footer');
 
-if (document.body.scrollHeight <= window.innerHeight) {
-    footer.classList.add('visible');
-}
-
-window.addEventListener('scroll', () => {
-    if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+    if (document.body.scrollHeight <= window.innerHeight) {
         footer.classList.add('visible');
     }
+
+    window.addEventListener('scroll', () => {
+        if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+            footer.classList.add('visible');
+        }
+    });
 });
